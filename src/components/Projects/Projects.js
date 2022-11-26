19d07a25d477ff6dd9d4bb1c32ac50f73cc7a1c6
NeverMind109 <vladgladkih123@gmail.com>
2022-11-26 21:13:58 +0300

    Finished:TimeLine, Accomplishments, Footer Comp-ts

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -22,35 +22,34 @@ import { projects } from "../../constants/constants";
 
 const Projects = () => (
   <Section nopadding id="projects">
-    <sectionDivider>
-      <sectionTitle main>Projects</sectionTitle>
-      <GridContainer>
-        {projects.map(
-          ({ id, image, title, description, tags, source, visit }) => (
-            <BlogCard key={id}>
-              <Img src={image} />
-              <TitleContent>
-                <HeaderThree title={title}></HeaderThree>
-                <Hr />
-              </TitleContent>
-              <CardInfo>{description}</CardInfo>
-              <div>
-                <TitleContent>Stack</TitleContent>
-                <TagList>
-                  {tags.map((tag, idx) => (
-                    <Tag key={idx}>{tag}</Tag>
-                  ))}
-                </TagList>
-              </div>
-              <UtilityList>
-                <ExternalLinks href={visit}>Code</ExternalLinks>
-                <ExternalLinks href={source}>Source</ExternalLinks>
-              </UtilityList>
-            </BlogCard>
-          )
-        )}
-      </GridContainer>
-    </sectionDivider>
+    <SectionDivider />
+    <SectionTitle main>Projects</SectionTitle>
+    <GridContainer>
+      {projects.map(
+        ({ id, image, title, description, tags, source, visit }) => (
+          <BlogCard key={id}>
+            <Img src={image} />
+            <TitleContent>
+              <HeaderThree title={title}></HeaderThree>
+              <Hr />
+            </TitleContent>
+            <CardInfo>{description}</CardInfo>
+            <div>
+              <TitleContent>Stack</TitleContent>
+              <TagList>
+                {tags.map((tag, idx) => (
+                  <Tag key={idx}>{tag}</Tag>
+                ))}
+              </TagList>
+            </div>
+            <UtilityList>
+              <ExternalLinks href={visit}>Code</ExternalLinks>
+              <ExternalLinks href={source}>Source</ExternalLinks>
+            </UtilityList>
+          </BlogCard>
+        )
+      )}
+    </GridContainer>
   </Section>
 );
 
